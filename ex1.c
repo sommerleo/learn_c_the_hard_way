d0866050d78fa29fdb257de5803127cd9c18752d
Léo Sommer <leosommer992@hotmail.com>
2024-08-15 21:16:42 -0300

    Initial commit, exercise 1

---
+++ ex1.c
@@ -0,0 +1,13 @@
+// This is the beginning of my C Language Learning from the book Learn C The Hard Way, Leo Sommer.
+#include <stdio.h>
+
+/* This is a comment. */
+int main(int argc, char *argv[])
+{
+	int distance = 256;
+	// this is also a comment
+	printf("You are %d miles away.\n", distance);
+
+	return 0;
+}
+
